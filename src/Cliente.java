e61c85cfb789f54374d79c3d6615160e1daef2df
SERGIO CANALES MARTINEZ <s.canales@sis.virtual.uniandes.edu.co>
2019-10-25 09:23:47 -0500

    hasta certificado digital

--- src/Cliente.java
+++ src/Cliente.java
@@ -9,29 +9,26 @@ import java.security.NoSuchAlgorithmException;
 
 public class Cliente{
 
-    private Socket connection;
+    private static Socket connection;
     private final static String padding = "AES/ECB/PKCS5Padding";//algoritmos y formato
-    private FileOutputStream archivoDeSalida;
-    private ObjectOutputStream oos;
-    private FileInputStream archivoDeEntrada;
-    private ObjectInputStream ois;
+    //private FileOutputStream archivoDeSalida;
+    //private ObjectOutputStream oos;
+    //private FileInputStream archivoDeEntrada;
+    //private ObjectInputStream ois;
     private static KeyGenerator keyGen;
     private static SecretKey KS;
-    private PrintWriter pw;
+    private static PrintWriter pw;
+    private static BufferedReader bf;
+    private static InputStreamReader in;
 
 
 
     public Cliente(){
-        try {
-            connection = new Socket("localhost" , 6789);
-            pw = new PrintWriter(connection.getOutputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
     }
 
 
-    private static byte[] cifrar(SecretKey KS, String m){
+    private static byte[] cifrarSimetrico(SecretKey KS, String m){
         try {
 
             Cipher cifrador = Cipher.getInstance(padding);
@@ -52,7 +49,7 @@ public class Cliente{
         }
     }
 
-    private static byte[] descifrar(byte[] mCifrado, SecretKey KS){
+    private static byte[] descifrarSimetrico(byte[] mCifrado, SecretKey KS){
         try {
             Cipher descifrador = Cipher.getInstance(padding);
             descifrador.init(Cipher.DECRYPT_MODE,KS);
@@ -79,7 +76,8 @@ public class Cliente{
         System.out.println(s);
     }
 
-    private static void generate(){//genera llaves con algoritmo AES
+    //genera llaves con algoritmo AES
+    private static void generateSimetricKey(){
         try {
             keyGen = KeyGenerator.getInstance("AES");
             KS = keyGen.generateKey();
@@ -89,14 +87,76 @@ public class Cliente{
     }
 
     public static void main(String args[]){
-        generate();
-        String s = "esto lo quiero cifrar";
-        byte[] mCifrado = cifrar(KS,s);
-        imprimirBytes(mCifrado);
-        byte[] mDescifrado = descifrar(mCifrado,KS);
-        imprimirBytes(mDescifrado);
-        System.out.println(new String(mDescifrado));
 
+        //String s = "esto lo quiero cifrar";
+        //byte[] mCifrado = cifrar(KS,s);
+        //imprimirBytes(mCifrado);
+        //byte[] mDescifrado = descifrar(mCifrado,KS);
+       // imprimirBytes(mDescifrado);
+        //System.out.println(new String(mDescifrado));
+
+        //1: establecer conexión
+        alistarConexion();
+        pw.println("HOLA");
+        pw.flush();
+
+        //leer OK
+        try {
+            System.out.println(bf.readLine());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //2: mandar algoritmos en orden- simétrico, asimétrico y hmac
+        String algoritmos = "ALGORITMOS:AES:RSA:HMACSHA256";
+        pw.println(algoritmos);
+        pw.flush();
+
+        try {
+
+            //leer OK
+            System.out.println(bf.readLine());
+
+            //leer certificado digital
+            String CD = bf.readLine();
+            System.out.println(CD);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //obtener llave de sesión a través del CD usando el estándar X509
+
+
+
+
+
+        //Cerrar
+        pw.close();
+        try {
+            bf.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+    private static void alistarConexion() {
+        try {
+
+            connection = new Socket("localhost", 6789);
+            pw = new PrintWriter(connection.getOutputStream());
+            in = new InputStreamReader(connection.getInputStream());
+            bf = new BufferedReader(in);
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
